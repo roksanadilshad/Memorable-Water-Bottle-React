41320a93e1852c7f72b7213393c422fe6c9edad4
Roksana Dilshad <roksanadilshad@gmail.com>
2025-09-21 14:49:34 +0600

    Add buitton

--- src/components/bottles/Bottle/Bottle.jsx
+++ src/components/bottles/Bottle/Bottle.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import './Bottle.css'
 
-const Bottle = ({bottle}) => {
-    console.log(bottle);
+const Bottle = ({bottle,  handlerBottle}) => {
+    //console.log(bottle);
     
-    const {img,name, price, quantity, ratings, shipping, stok } = bottle;
+    const {img,name, price, quantity, ratings, shipping, stock ,seller} = bottle;
     return (
         <div className='card'>
-            <img src={img} alt="" />
-            <h4>name:{name}</h4>
+            <h2>{seller}</h2>
+            <img src={img} alt="" className='bottle-img'/>
+            <h4>Name:{name}</h4>
             <p>price:{price} </p>
             <p>Shipping:{shipping} </p>
-            <p>Stok:{stok} </p>
+            <p>Stok:{stock} </p>
             <p>Quantity:{quantity} </p>
             <p>Ratings:{ratings} </p>
+            <button onClick={() => {handlerBottle(bottle)}}>Buy Now</button>
             <p></p>
             
         </div>
--- src/components/bottles/Bottles.jsx
+++ src/components/bottles/Bottles.jsx
@@ -1,16 +1,25 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Bottle from './bottle/bottle';
+import './Bottles.css'
 
 const Bottles = ({bottlePromise}) => {
     const bottleCome = use(bottlePromise)
-    console.log(bottleCome);
-    
+    //console.log(bottleCome);
+    const [piku, setpiku] = useState([]);
+    const handlerBottle = (bottle) =>{
+       console.log(bottle);
+       
+        
+    }
     return (
         <div>
             <h3>Bottles are {bottleCome.length}</h3>
+            <div className='card-container'>
+
             {
-                bottleCome.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                bottleCome.map(bottle => <Bottle  handlerBottle={ handlerBottle} key={bottle.id} bottle={bottle}></Bottle>)
             }
+            </div>
         </div>
     );
 };
